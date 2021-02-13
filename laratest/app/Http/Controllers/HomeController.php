5f48c1b1437bdd20dd529cf71bc7876ff416bcc8
webforshafayet <shohan123boss@gmail.com>
2021-02-13 15:03:36 +0600

    single data pass to homepage

--- laratest/app/Http/Controllers/HomeController.php
+++ laratest/app/Http/Controllers/HomeController.php
@@ -2,13 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Faker\Guesser\Name;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+          // return view('home.index');
 
-        return view('home.index');
+       // return view('home.index', ['name'=> 'xyz', 'id'=>12]);
+
+
+// alternative---------------------------------------------
+
+
+
+
+        // return view('home.index')
+        //       ->with('name', 'alamin')
+        //       ->with('id', '12');
+// alternative---------------------------------------------
+        $name="shafayet";
+        $id="12";
+        //  return view('home.index')
+        //          ->withName($name)
+        //          ->withId($id);
+// alternative---------------------------------------------
+        return view('home.index', compact('id', 'name'));
 
     }
     public function userlist(){
--- laratest/resources/views/home/index.blade.php
+++ laratest/resources/views/home/index.blade.php
@@ -4,10 +4,11 @@
     <title>Home Page</title>
 </head>
 <body>
-    <h1>Welcome home! </h1>
+    <h1>Welcome home! {{ $name }}  </h1>
     <a href="/create">Create user</a> |
     <a href="/home/userlist">View user list</a> |
     <a href="/logout">logout</a>
+     <br>{{ $id }}
     <br>
 
 
